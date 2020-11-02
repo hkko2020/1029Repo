a3c72cedb241bf807e933b9ef53feceaaee8aee2
hkko <hkko@CurieNote>
2020-11-02 10:01:10 -0800

    create new class: Demo

---
+++ MgrLeader/src/Demo.java
@@ -0,0 +1,17 @@
+
+public class Demo {
+
+	private int num;
+	
+	public Demo() {
+		num = 0;
+	}
+	
+	public void setNumber(int num) {
+		this.num = num;
+	}
+	
+	public int getNumber() {
+		return num;
+	}
+}
